240747d87afdd7b50507cbef4df7a2b8471f8262
ashroaban003 <ratnakumar48070@gmail.com>
2025-11-20 21:00:08 +0530

    Initial commit

---
+++ src/main/kotlin/com/plooding/demo/Kotlin1Application.kt
@@ -0,0 +1,11 @@
+package com.plooding.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlin1Application
+
+fun main(args: Array<String>) {
+	runApplication<Kotlin1Application>(*args)
+}
---
+++ src/test/kotlin/com/plooding/demo/Kotlin1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plooding.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kotlin1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
